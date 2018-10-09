396415625d228785ebe8839737c0d32765427a00
NewAndycul96 <andycul96@yahoo.com>
2018-10-09 10:13:11 -0500

    Photos is working

--- Photos/PhotosViewController.swift
+++ Photos/PhotosViewController.swift
@@ -9,22 +9,80 @@
 import UIKit
 
 class PhotosViewController: UIViewController {
-
+    
+    let imagePicker = UIImagePickerController()
+    var imageView = UIImageView()
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func openCamera(_ sender: Any) {
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+            print("camera not supported by this device")
+            return
+        }
+        imagePicker.sourceType = .camera
+        imagePicker.delegate = self
+        imagePicker .allowsEditing = true
+        
+        present(imagePicker, animated: true)
     }
-    */
-
+//    func openCamera() {
+//        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+//            print("camera not supported by this device")
+//            return
+//        }
+//        imagePicker.sourceType = .camera
+//        imagePicker.delegate = self
+//        imagePicker .allowsEditing = true
+//
+//        present(imagePicker, animated: true)
+//    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image  = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            return
+        }
+        imageView.image = image
+    }
+    @IBAction func openPhotoLibrary(_ sender: Any) {
+        guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else {
+            print("Unable to open Photo Library")
+            return
+        }
+        
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        
+        present(imagePicker, animated: true)
+    }
+//    func openPhotoLibrary() {
+//        guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else {
+//            print("Unable to open Photo Library")
+//            return
+//        }
+//        
+//        imagePicker.sourceType = .photoLibrary
+//        imagePicker.delegate = self
+//        
+//        present(imagePicker, animated: true)
+//    }
+}
+extension PhotosViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    private func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        defer {
+            picker.dismiss(animated: true)
+        }
+        
+        print(info)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        defer {
+            picker.dismiss(animated: true)
+        }
+        
+        print("did cancel")
+    }
 }
