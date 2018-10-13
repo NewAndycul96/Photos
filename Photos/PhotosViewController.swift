f955bc1d54beaa3cd019c68d87fcd992d5c82d16
NewAndycul96 <andycul96@yahoo.com>
2018-10-12 19:01:45 -0500

    Latest

--- Photos/PhotosViewController.swift
+++ Photos/PhotosViewController.swift
@@ -8,16 +8,14 @@
 
 import UIKit
 
-class PhotosViewController: UIViewController {
+class PhotosViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     let imagePicker = UIImagePickerController()
     var imageView = UIImageView()
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        
     }
-    
     @IBAction func openCamera(_ sender: Any) {
         guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
             print("camera not supported by this device")
@@ -25,26 +23,17 @@ class PhotosViewController: UIViewController {
         }
         imagePicker.sourceType = .camera
         imagePicker.delegate = self
-        imagePicker .allowsEditing = true
+        imagePicker .allowsEditing = false
         
         present(imagePicker, animated: true)
     }
-//    func openCamera() {
-//        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
-//            print("camera not supported by this device")
-//            return
-//        }
-//        imagePicker.sourceType = .camera
-//        imagePicker.delegate = self
-//        imagePicker .allowsEditing = true
-//
-//        present(imagePicker, animated: true)
-//    }
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        guard let image  = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
-            return
+        if let image  = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = image
         }
-        imageView.image = image
+        else {
+            print("Error")
+        }
     }
     @IBAction func openPhotoLibrary(_ sender: Any) {
         guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else {
@@ -54,35 +43,24 @@ class PhotosViewController: UIViewController {
         
         imagePicker.sourceType = .photoLibrary
         imagePicker.delegate = self
-        
+        imagePicker.allowsEditing = false
         present(imagePicker, animated: true)
     }
-//    func openPhotoLibrary() {
-//        guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else {
-//            print("Unable to open Photo Library")
-//            return
+}
+//extension PhotosViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+//    private func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+//        defer {
+//            picker.dismiss(animated: true)
 //        }
-//        
-//        imagePicker.sourceType = .photoLibrary
-//        imagePicker.delegate = self
-//        
-//        present(imagePicker, animated: true)
+//
+//        print(info)
 //    }
-}
-extension PhotosViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    private func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        defer {
-            picker.dismiss(animated: true)
-        }
-        
-        print(info)
-    }
-    
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        defer {
-            picker.dismiss(animated: true)
-        }
-        
-        print("did cancel")
-    }
-}
+//
+//    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+//        defer {
+//            picker.dismiss(animated: true)
+//        }
+//
+//        print("did cancel")
+//    }
+//}
